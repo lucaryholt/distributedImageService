3bce2951812089bda6e4b8cc677aabd0d8f7080b
Luca <lucaryholt@gmail.com>
2020-04-28 10:38:14 +0200

    more logic in distributor

--- src/main/java/Service/Distributor.java
+++ src/main/java/Service/Distributor.java
@@ -3,12 +3,20 @@ package Service;
 import Model.Job;
 
 import java.awt.image.BufferedImage;
-import java.util.concurrent.BlockingDeque;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Distributor {
 
-    private BlockingDeque<Job> googleQueue;
-    private BlockingDeque<Job> amazonQueue;
+    private Queue<Job> googleQueue = new LinkedList<>();
+    private Queue<Job> amazonQueue = new LinkedList<>();
+
+    public Distributor() {
+        Thread googleThread = new Thread(new GoogleQueueHandler(googleQueue));
+        Thread amazonThread = new Thread(new AmazonQueueHandler(amazonQueue));
+        googleThread.start();
+        amazonThread.start();
+    }
 
     public synchronized void addJob(int id, String service, BufferedImage image){
         Job job = new Job(id, image);
@@ -20,4 +28,48 @@ public class Distributor {
         }
     }
 
+}
+
+class GoogleQueueHandler implements Runnable{
+
+    private Queue<Job> queue;
+
+    public GoogleQueueHandler(Queue<Job> queue) {
+        this.queue = queue;
+    }
+
+    private void processJob(){
+        if(queue.peek() != null){
+            //Do something with job
+        }
+    }
+
+    @Override
+    public void run() {
+        while(true){
+            processJob();
+        }
+    }
+}
+
+class AmazonQueueHandler implements Runnable{
+
+    private Queue<Job> queue;
+
+    public AmazonQueueHandler(Queue<Job> queue) {
+        this.queue = queue;
+    }
+
+    private void processJob(){
+        if(queue.peek() != null){
+            //Do something with job
+        }
+    }
+
+    @Override
+    public void run() {
+        while(true){
+            processJob();
+        }
+    }
 }
