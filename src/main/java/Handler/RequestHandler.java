5beb2c4763ee77d350bc767a2e11ff7ca7bb6234
Luca <lucaryholt@gmail.com>
2020-04-28 10:17:59 +0200

    more logic in requesthandler

--- src/main/java/Handler/RequestHandler.java
+++ src/main/java/Handler/RequestHandler.java
@@ -1,14 +1,22 @@
 package Handler;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import Service.Distributor;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class RequestHandler {
 
-
+    public void initianeReceiveClient(int port, SessionManager sessionManager, Distributor distributor){
+        Thread thread = new Thread(new ReceiveClient(port, sessionManager, distributor));
+        thread.start();
+    }
 
 }
 
@@ -17,9 +25,14 @@ class ReceiveClient implements Runnable{
 
     private ServerSocket serverSocket;
     private SessionManager sessionManager;
+    private JSONParser jsonParser;
+    private Distributor distributor;
+    private int id = 0;
 
-    public ReceiveClient(int port, SessionManager sessionManager){
+    public ReceiveClient(int port, SessionManager sessionManager, Distributor distributor){
         this.sessionManager = sessionManager;
+        this.distributor = distributor;
+        this.jsonParser = new JSONParser();
         try{
             serverSocket = new ServerSocket(port);
         }catch(IOException e){
@@ -28,28 +41,44 @@ class ReceiveClient implements Runnable{
     }
 
     private void receive(){
-        while(true){
-            try {
-                Socket socket = serverSocket.accept();
-                receiveRequest(socket);
-                //sessionHandler.addClient(id, socket);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    private void receiveRequest(Socket socket){
         try {
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
+            Socket socket = serverSocket.accept();
+            int id = getId();
+            receiveRequest(socket, id);
+            sessionManager.addClient(socket, id);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    private void receiveRequest(Socket socket, int id){
+        try {
+            try {
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                JSONObject jsonObject = (JSONObject) jsonParser.parse(bufferedReader.readLine());
+
+                String service = (String) jsonObject.get("service");
+                InputStream in = new ByteArrayInputStream((byte[]) jsonObject.get("image"));
+                BufferedImage imageFromBytes = ImageIO.read(in);
+
+                //distributor.addJob(id, service, imageFromBytes);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private int getId(){
+        id++;
+        return id;
+    }
+
     @Override
     public void run() {
-
+        while(true){
+            receive();
+        }
     }
 }
