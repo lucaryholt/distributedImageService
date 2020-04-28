c73a41803e49218cdc3e02c902693605d5368d17
Luca <lucaryholt@gmail.com>
2020-04-28 11:00:40 +0200

    tilføjede kommentare

--- src/main/java/Service/Distributor.java
+++ src/main/java/Service/Distributor.java
@@ -6,19 +6,21 @@ import java.awt.image.BufferedImage;
 import java.util.LinkedList;
 import java.util.Queue;
 
-
+//Luca
 public class Distributor {
 
     private Queue<Job> googleQueue = new LinkedList<>();
     private Queue<Job> amazonQueue = new LinkedList<>();
 
     public Distributor() {
+        //Starter de to threads til håndtering af de to køer
         Thread googleThread = new Thread(new GoogleQueueHandler(googleQueue));
         Thread amazonThread = new Thread(new AmazonQueueHandler(amazonQueue));
         googleThread.start();
         amazonThread.start();
     }
 
+    //Tager imod jobs fra RequestHandleren og deler dem ud til de rigtige køer
     public synchronized void addJob(int id, String service, BufferedImage image){
         Job job = new Job(id, image);
         switch (service){
@@ -39,6 +41,7 @@ class GoogleQueueHandler implements Runnable{
         this.queue = queue;
     }
 
+    //TODO Den her skal hive forreste Job og håndtere det og til sidst sende det til responseHandleren som result
     private void processJob(){
         if(queue.peek() != null){
             //Do something with job
