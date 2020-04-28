f168ec7be775de3c550c1b73b53f1a96351fcdb2
Asbjørn Stahl Garne <asbjoern@garne.dk>
2020-04-28 11:11:09 +0200

    added AWS code

--- src/main/java/Service/Distributor.java
+++ src/main/java/Service/Distributor.java
@@ -3,9 +3,22 @@ package Service;
 import Model.Job;
 
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
-
+import com.amazonaws.services.rekognition.AmazonRekognition;
+import com.amazonaws.services.rekognition.AmazonRekognitionClientBuilder;
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.services.rekognition.model.AmazonRekognitionException;
+import com.amazonaws.services.rekognition.model.DetectLabelsRequest;
+import com.amazonaws.services.rekognition.model.DetectLabelsResult;
+import com.amazonaws.services.rekognition.model.Image;
+import com.amazonaws.services.rekognition.model.Label;
+import com.amazonaws.util.IOUtils;
 
 public class Distributor {
 
@@ -63,8 +76,34 @@ class AmazonQueueHandler implements Runnable{
 
     private void processJob(){
         if(queue.peek() != null){
-            //Do something with job
+            //See source: https://docs.aws.amazon.com/rekognition/latest/dg/images-bytes.html
 
+            String photo = "input.jpg";
+
+            ByteBuffer imageBytes;
+            try (InputStream inputStream = new FileInputStream(new File(photo))) {
+                imageBytes = ByteBuffer.wrap(IOUtils.toByteArray(inputStream));
+
+
+            AmazonRekognition rekognitionClient = AmazonRekognitionClientBuilder.defaultClient();
+
+            DetectLabelsRequest request = new DetectLabelsRequest()
+                    .withImage(new Image()
+                            .withBytes(imageBytes))
+                    .withMaxLabels(10)
+                    .withMinConfidence(77F);
+
+            DetectLabelsResult result = rekognitionClient.detectLabels(request);
+            List<Label> labels = result.getLabels();
+
+            System.out.println("Detected labels for " + photo);
+            for (Label label: labels) {
+                System.out.println(label.getName() + ": " + label.getConfidence().toString());
+            }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 
